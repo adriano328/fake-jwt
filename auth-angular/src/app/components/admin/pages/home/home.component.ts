4047d22df29904737f7bb5703461a9aee34425fa
adriano328 <lucasadrianodias@gmail.com>
2022-08-16 10:58:17 -0400

    Token pagina sing

--- auth-angular/src/app/components/admin/pages/home/home.component.ts
+++ auth-angular/src/app/components/admin/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public logout(){
+    this.authService.logout();
   }
 
 }
